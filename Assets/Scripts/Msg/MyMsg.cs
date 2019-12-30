3455d8e55b8bf30587a9b2b4d70ab513f5b762b3
admin007 <haha@163.com>
2019-12-31 02:49:52 +0800

    quene msg

---
+++ Assets/Scripts/Msg/MyMsg.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMsg : MonoBehaviour
+{
+    public string  localName;
+    public virtual void  Start()
+    {
+        MyMsgQueue.GetInistance().OnSendMsg += Send;
+
+        MyMsgQueue.GetInistance().OnReviceMsg += Recive;
+
+        MyMsgQueue.GetInistance().MysMsg += Send;
+
+        localName = gameObject.name;
+    }
+    
+    public void SendMsg(string msg)
+    {
+        var str = localName + ":" + msg;
+       MyMsgQueue.GetInistance().MysMsg(str);
+    }
+
+    protected virtual void Send(string msg)
+    {
+       var str = msg.Split(':');
+        if(!str[0].Equals(gameObject.name))
+        print(gameObject.name + ":"+ str[1]);
+    }
+    
+    protected virtual string Recive(string msg)
+    {
+        print(msg);
+        return msg;
+    }
+}
---
+++ Assets/Scripts/Msg/MyMsgQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class MyMsgQueue
+{
+    private static MyMsgQueue inistance = null;
+
+    public static MyMsgQueue GetInistance()
+    {
+        if (inistance == null) inistance = new MyMsgQueue();
+       
+        return inistance;
+    }
+
+    private bool isalive;
+
+    private MyMsgQueue()
+    {
+
+    }
+    public Func<int, string> _Fun;
+    public Action<string> _Action;
+    public delegate void sMsg(string str);
+    public delegate string rMsg(string str);
+    public event sMsg OnSendMsg;
+    public event rMsg OnReviceMsg;
+
+    public sMsg MysMsg;
+
+    public void SendMsg(string msg)
+    {
+        Debug.Log("委托发送：" + msg);
+        OnSendMsg?.Invoke(msg);
+    }
+
+    public string ReviceMsg(string str)
+    {
+        Debug.Log("委托接收：" + str);
+        return OnReviceMsg?.Invoke(str);
+    }
+}
