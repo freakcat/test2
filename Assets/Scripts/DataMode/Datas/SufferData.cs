43382f3a66cc25e5bab920f0feabe5be8c17e439
freakcat <1193295918@qq.com>
2019-12-30 15:17:17 +0800

    add mysql

---
+++ Assets/Scripts/DataMode/Datas/SufferData.cs
@@ -0,0 +1,16 @@
+﻿namespace DataMode.Datas
+{
+   [System.Serializable]
+   public class SufferData
+   {
+      public string name;
+      public int sex;
+      public int age;
+      public int type;
+      public float weight;
+      public string guardian;
+      public long phone;
+      public string maintell;
+      public string avatar;
+   }
+}
