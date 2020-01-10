3e169f705da9da195ab9e0faec009ec5ee6908f7
freakcat <1193295918@qq.com>
2020-01-10 12:43:20 +0800

    ...

---
+++ Assets/Scripts/Test/NewtonJsonTest2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+
+namespace Test
+{
+    public class NewtonJsonTest2: MonoBehaviour
+    {
+        private void Start()
+        {
+            Test0();
+        }
+
+        public void Test0()
+        {
+            JArray array = new JArray();
+            array.Add("Manual text");
+            array.Add(new DateTime(2000, 5, 23));
+
+            JObject o = new JObject();
+            o["MyArray"] = array;
+
+            string json = o.ToString();
+ 
+            JArray arrayRoot = new JArray();
+            arrayRoot.Add(array);
+            JObject oRoot = new JObject();
+            oRoot["Root"] = arrayRoot;
+            string jsonRoot = oRoot.ToString();
+ 
+            print(jsonRoot);
+        }
+
+    }
+}
